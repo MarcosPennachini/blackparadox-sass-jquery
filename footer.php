aaa48a60e43c383c25b372db333c030b302d3211
MarcosPenna <marcoskpo_penpun@hotmail.com>
2021-09-29 13:43:52 -0300

    Setting up repository

---
+++ footer.php
@@ -0,0 +1,11 @@
+<footer>
+
+</footer>
+
+<script src="./dist/js/jquery-min.js"></script>
+<script src="./dist/js/bootstrap.bundle.min.js"></script>
+<script src="./dist/js/main-min.js"></script>
+
+</body>
+
+</html>

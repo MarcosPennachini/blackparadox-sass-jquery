db212f6769b47903abe45fccec0faff7c9a3c01a
MarcosPenna <marcoskpo_penpun@hotmail.com>
2021-09-29 21:59:18 -0300

    Adding slippery component for images slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,12 @@
-console.log('HOla');
-$('#portfolioLink').click(function (e) {
-  e.preventDefault;
-  $('#render').load('portfolio.php');
+// console.log('HOla');
+// $('#portfolioLink').click(function (e) {
+//   e.preventDefault;
+//   $('#render').load('portfolio.php');
+// });
+
+$(document).ready(function () {
+  $('#slippry').slippry({
+    captions: false,
+    pager: false,
+  });
 });
